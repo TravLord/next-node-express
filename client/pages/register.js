d9d759efb6262669b210899d5bace4c4edf538f2
TravLord <92835555+TravLord@users.noreply.github.com>
2022-09-01 18:41:10 -0600

    test2

--- client/pages/register.js
+++ client/pages/register.js
@@ -86,7 +86,7 @@ const Register = () => {
             {registerForm()}
             {/* {JSON.stringify(state)}  This will show our state values in real time as they are updated */}
         </div>
-        yellow
+        yellow chicken
         {success && showSuccessMessage(success)}
         {error && showErrorMessage(error)}
     </Layout>
